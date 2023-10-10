ff67e00a5a0d9a1c96527f81b18b297a706adb1d
Filippo <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-10-10 17:42:56 +0200

    03.density_maps.R

--- 03.density_maps.R
+++ 03.density_maps.R
@@ -16,9 +16,35 @@ cl<-colorRampPalette(c("black","red","orange","yellow"))(100)
 # devo usare "" altrimenti R non li riconosce
 # ho bisogno di un array c() per usare tutti e 4 i colori
 # (100) è il numero di colori che mi servono per passare da un colore all'altro
+# per esempio, con (4), non c'è continuità tra un colore all'altro, non c'è un gradiente
 # devo assegnarlo ad un oggetto che chiamo cl in modo da richiamarlo
 
 plot(densitymap,col=cl)
+
 # il giallo è il colore che impatta maggiormente, quindi va usato con cautela perché il primo che viene visto in una mappa
 # va quindi messo in alto
+
+elevation<-bei.extra[[1]]
+
+# funzione par mi serve per mostrare due o piu grafici:multiframe
+# (1,2) devo usare c() perché devo dire che è un array
+# c(righe,colonne)
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elevation)
+points(bei,cex=.2)
+
+# fare 3 grafici accanto
+par(mfrow=c(1,3))
+plot(bei,cex=.2)
+plot(densitymap,col=cl)
+plot(elevation,col=cl)
+
+
+
+
+
+
+
+
 
