6ad8673de2f947e1ed2988833af58751b5774c23
Filippo <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-10-10 11:50:29 +0200

    02.populations.R

---
+++ 02.populations.R
@@ -0,0 +1,35 @@
+# Code related to population ecology
+
+# A package is needed for point pattern analysis
+#everytime we take something that is outside of R we need to use the brackets
+install.packages("spatstat")
+library(spatstat) # to check if it has been installed
+
+#the dataset is called 'bei' and it's inside the package spatstat
+# data description:
+#aways put the link as a comment
+# https://CRAN.R-project.org/package=spatstat
+
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=.2)
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# there's an additional datasets in the package
+bei.extra
+plot(bei.extra)
+
+# let's use only part of the dataset: elev
+plot(bei.extra$elev)
+elevation <- bei.extra$elev
+plot(elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]
+plot(elevation2)
