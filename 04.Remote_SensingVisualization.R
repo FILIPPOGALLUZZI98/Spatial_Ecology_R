34bf8ce35a6c3d6166142baee1d621d594d4cbc8
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-10-26 17:42:23 +0200

    Update 04.Remote_SensingVisualization.R

--- 04.Remote_SensingVisualization.R
+++ 04.Remote_SensingVisualization.R
@@ -14,7 +14,9 @@ b2<-im.import("sentinel.dolomites.b2.tif")
 # le bande differiscono in base alla lunghezza d'onda di osservazione
 b2 # per vedere le caratteristiche
 
-clb<-colorRampPalette(c("darkgrey","grey","lightgrey"))(100)
-plot(b2,col=clb)
+cl<-colorRampPalette(c("darkgrey","grey","lightgrey"))(100)
+plot(b2,col=cl)
 
-
+# import the green band from sentinel-2 (band 3)
+b3<-im.import("sentinel.dolomites.b3.tif")
+plot(b3,col=cl)
