563589b0636adeff5ac3536f6c9a60baebcc1018
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-10-26 16:38:41 +0200

    Create 04.Remote_SensingVisualization.R

---
+++ 04.Remote_SensingVisualization.R
@@ -0,0 +1,20 @@
+# RS data
+# This is a script to visualize satellite data
+
+library(devtools) # packages in R are also called libraries
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+library(imageRy)
+library(terra)
+
+#list the data
+im.list()
+# "sentinel.dolomites.b2.tif" questo indica la banda 2 del satellite
+b2<-im.import("sentinel.dolomites.b2.tif")
+# le bande differiscono in base alla lunghezza d'onda di osservazione
+b2 # per vedere le caratteristiche
+
+
+
+
+
