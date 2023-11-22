8eefdfd3c90f42154f26908f7b37d122335fe0a6
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-22 16:46:39 +0100

    Update 01.Plots_and_Maps.R

--- 01.Plots_and_Maps.R
+++ 01.Plots_and_Maps.R
@@ -1,42 +1,3 @@
-# CODE RELATED TO POPULATION ECOLOGY
-
-install.packages("spatstat")
-# quando si esce da R, come per il caso dell'installaazione dei pacchetti, dobbiamo usare ""
-library(spatstat) # to check if it has been installed
-
-#the dataset is called 'bei' and it's inside the package spatstat
-# dataset bei--> tropical forest trees
-# data description:
-#aways put the link as a comment
-# https://CRAN.R-project.org/package=spatstat
-
-bei
-
-# plotting the data
-plot(bei)
-
-# changing dimension - cex
-plot(bei, cex=.2)
-
-# changing the symbol - pch
-plot(bei, cex=.2, pch=19)
-
-# there's an additional datasets in the package
-bei.extra
-plot(bei.extra)
-
-# let's use only part of the dataset: elev
-plot(bei.extra$elev)
-elevation <- bei.extra$elev
-plot(elevation)
-
-# second method to select elements
-elevation2 <- bei.extra[[1]]
-plot(elevation2)
-
-
-#####################################################################################################
-#####################################################################################################
 # WHY POPULATION DISPERSE OVER THE LANDSCAPE IN A CERTAIN MANNER?
 
 library(sdm)
@@ -129,61 +90,6 @@ plot(precmap)
 points(pres,cex=1,col="black")
 plot(vegemap)
 points(pres,cex=1,col="black")
-
-
-#####################################################################################################
-#####################################################################################################
-library(spatstat)
-
-bei
-# vogliamo passare ad una funzione di densità
-densitymap<-density(bei)
-
-# we aren't dealing with points anymore, but with pixels
-# vogliamo sovrapporre alla mappa di densità anche la mappa dei punti
-# questa funzione si chiama points
-points(bei,cex=.4)
-
-# vogliamo cambiare i colori della mappa
-# la funzione si chiama colorRampPalette
-cl<-colorRampPalette(c("black","red","orange","yellow"))(100)
-# devo usare "" altrimenti R non li riconosce
-# ho bisogno di un array c() per usare tutti e 4 i colori
-# (100) è il numero di colori che mi servono per passare da un colore all'altro
-# per esempio, con (4), non c'è continuità tra un colore all'altro, non c'è un gradiente
-# devo assegnarlo ad un oggetto che chiamo cl in modo da richiamarlo
-
-plot(densitymap,col=cl)
-
-# il giallo è il colore che impatta maggiormente, quindi va usato con cautela perché il primo che viene visto in una mappa
-# va quindi messo in alto
-
-elevation<-bei.extra[[1]]
-
-# funzione par mi serve per mostrare due o piu grafici:multiframe
-# (1,2) devo usare c() perché devo dire che è un array
-# c(righe,colonne)
-par(mfrow=c(1,2))
-plot(densitymap)
-plot(elevation)
-points(bei,cex=.2)
-
-# fare 3 grafici accanto
-par(mfrow=c(1,3))
-plot(bei,cex=.2)
-plot(densitymap,col=cl)
-plot(elevation,col=cl)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
