7efa3b137b5cb567ecb0b4919deb69132e1d9d96
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-22 18:08:54 +0100

    Update and rename 001. to 302.DVI.R

---
+++ 302.DVI.R
@@ -0,0 +1,16 @@
+# NORMALIZED VEGETATION INDEX
+library(imageRy)
+library(terra)
+library(ggplot2)
+library(viridis)
+
+# Lavoriamo con i dati 'matogrosso'
+im.list()
+m1992<-im.import("matogrosso_l5_1992219_lrg.jpg")
+
+
+
+
+
+
+
