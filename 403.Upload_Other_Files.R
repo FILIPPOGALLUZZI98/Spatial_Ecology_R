e2ef6e7b33ff106c922afe5977cbef840c8fec75
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-23 12:36:32 +0100

    Create 403.Upload_Other_Files.R

---
+++ 403.Upload_Other_Files.R
@@ -0,0 +1,7 @@
+###########################################################
+## Landsat (30m) and MODIS (>500m) data (NASA):
+https://www.youtube.com/watch?v=JN-P04Dkx48
+
+###########################################################
+## Copernicus (ESA):
+https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
