0dc34871aedb6b296ea2fd1d503a4013f03670c1
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-23 12:52:58 +0100

    Update and rename 401.Upload_Earth_Obs.R to 401.Earth_Observatory.R

--- 401.Earth_Observatory.R
+++ 401.Earth_Observatory.R
@@ -2,21 +2,14 @@
 https://earthobservatory.nasa.gov/
 
 ###########################################
+###### IMMAGINI DI PROVA 1
+##########################################
 # Upload immagine Naja City
+
 library(terra)
 
-# Dopo aver scaricato l'immagine in una cartella:
-# Percorso cartella --> "D:R_Studio/Immagini_Monitoring/Immagini_Prova"
-# SE HO SPOSTATO LA CARTELLA O L'HO RINOMINATA --> CONTROLLARE E CAMBIARE
-# Se invece metto "D:\R_Studio/Immagini_Monitoring/Immagini_Prova" non funziona, 
-# quindi fare attenzione a '/' dopo 'D:'
-# setwd --> set working directory
-setwd("D:R_Studio/Immagini_Monitoring/Immagini_Prova")
-
-# rast() --> per importare l'immagine, come 'im.import'
 img1<-rast("Naja_City_May_20_2003.jpg")
 img2<-rast("Naja_City_Ago_07_2023.jpg")
-# Su windows ricordare di aggiungere l'estensione alla fine perché la nasconde!!
 img1
 # Le immagini sono formate da 3 componenti, che corrispondono alle bande RGB
 
