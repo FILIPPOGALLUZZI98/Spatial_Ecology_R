6fd2f53b9ba79cf11065c38cd4e6bf8c1d45c1fa
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-23 12:36:10 +0100

    Update 402.Uploard_Sentinel-2.R

--- 402.Uploard_Sentinel-2.R
+++ 402.Uploard_Sentinel-2.R
@@ -63,14 +63,6 @@ Normalized Difference Water Index (NDWI) The normalized difference water index i
 # Twelve different classifications are provided including classes of clouds, vegetation, 
 # soils/desert, water and snow. It does not constitute a land cover classification map in a strict sense.
 
-###########################################################
-## Landsat (30m) and MODIS (>500m) data (NASA):
-https://www.youtube.com/watch?v=JN-P04Dkx48
-
-###########################################################
-## Copernicus (ESA):
-https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
-
 
 
 
