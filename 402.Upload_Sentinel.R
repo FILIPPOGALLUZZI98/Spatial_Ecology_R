1375c26e8a0176e7f0f50e9c09a57573714c8d78
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-23 12:03:47 +0100

    Update 402.Upload_Sentinel.R

--- 402.Upload_Sentinel.R
+++ 402.Upload_Sentinel.R
@@ -1,5 +1,10 @@
 ## Sentinel-2 data (10m, ESA):
-https://www.youtube.com/watch?v=KA2L4bDmo98
+https://dataspace.copernicus.eu/explore-data/data-collections/sentinel-data/sentinel-2
+# Cliccare su 'Explore Sentinel-2 Data' e si apre una mappa
+# Andare su 'Search'
+# Selezionare la porzione di superficie che voglio analizzare
+# Selezionare time-range e altre impostazioni 
+
 
 
 ## Landsat (30m) and MODIS (>500m) data (NASA):
@@ -7,6 +12,7 @@ https://www.youtube.com/watch?v=JN-P04Dkx48
 ## Copernicus (ESA):
 https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
 
+###########################################################
 
 
 
