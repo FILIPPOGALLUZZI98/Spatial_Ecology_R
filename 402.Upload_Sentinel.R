7e5e23024d53f6cbc1c3868df3bf6ef0047a8ad5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-23 11:02:30 +0100

    Update 402.Upload_Sentinel.R

--- 402.Upload_Sentinel.R
+++ 402.Upload_Sentinel.R
@@ -1,1 +1,31 @@
+## Sentinel-2 data (10m, ESA):
+https://www.youtube.com/watch?v=KA2L4bDmo98
+
+
+## Landsat (30m) and MODIS (>500m) data (NASA):
+https://www.youtube.com/watch?v=JN-P04Dkx48
+## Copernicus (ESA):
+https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
