570497740d358be0dccb25abd15c1c8dff27ea7f
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-23 11:01:51 +0100

    Update and rename 401.Upload_data.R to 401.Upload_Earth_Obs.R

--- 401.Upload_Earth_Obs.R
+++ 401.Upload_Earth_Obs.R
@@ -1,15 +1,6 @@
 # AVAILABLE DATA:
-
 ## Earth Observatory (NASA):
 https://earthobservatory.nasa.gov/
-## Sentinel-2 data (10m, ESA):
-https://www.youtube.com/watch?v=KA2L4bDmo98
-## Landsat (30m) and MODIS (>500m) data (NASA):
-https://www.youtube.com/watch?v=JN-P04Dkx48
-## Copernicus (ESA):
-https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
-
-
 
 ###########################################
 ###### IMMAGINI DI PROVA
