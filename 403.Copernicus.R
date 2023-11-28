eff3cc74ddd006e4d42740eb8639cba947824ae5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-28 17:15:54 +0100

    Update 403.Copernicus.R

--- 403.Copernicus.R
+++ 403.Copernicus.R
@@ -1,5 +1,21 @@
 ## Copernicus (ESA):
-https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+# Estensione dei dati è '.nc'
 
-##################################################################################
+library(terra)
+library(ncdf4)
+setwd("D:R_Studio/Immagini_Prova/Copernicus")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
