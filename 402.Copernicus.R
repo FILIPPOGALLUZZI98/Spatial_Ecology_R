e92f08fec2b7e22677ce60a69e7ea3faed44bb8b
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-11-28 17:49:05 +0100

    Update 402.Copernicus.R

--- 402.Copernicus.R
+++ 402.Copernicus.R
@@ -4,13 +4,18 @@
 
 library(terra)
 library(ncdf4)
-setwd("D:R_Studio/Immagini_Prova/Copernicus")
+setwd("D:R_Studio/Immagini_Monitoring/Copernicus")
 
+soil_moist<-rast("Soil_Moisture.nc")
+plot(soil_moist)
+# Ci sono due variabili, ssm (surface soil moisture) e ssm noise
+cl<-colorRampPalette(c("red","orange","yellow"))(100)
+plot(soil_moist[[1]], col=cl)
 
-
-
-
-
+# Voglio vedere solo una parte dell'immagine
+ext<-c(25.5,26,41,41)
+soil_moist_crop<-crop(soil_moist[[1]],ext)
+plot(soil_moist_crop,col=cl)
 
 
 
