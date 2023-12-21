498a56cb15d2b6034af3683a2727d089604de6f5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-21 17:15:49 +0100

    Update and rename 506.PCA.R to 406.PCA.R

---
+++ 406.PCA.R
@@ -0,0 +1,29 @@
+# Principal Component Analysis
+library(imageRy)
+library(viridis)
+library(terra)
+
+sent <- im.import("sentinel.png")
+sent.pca <- im.pca(sent)
+
+# measuring variability in the first PC:
+sd3pc <- focal(sent.pca[[1]], matrix(1/9, 3, 3), fun=sd)
+viridis <- colorRampPalette(viridis(7))(255)
+plot(sd3pc, col=viridis)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
