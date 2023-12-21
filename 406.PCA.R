e86b0313e5859a75a0b3db5d781ce8517b2b5ee5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-21 17:32:28 +0100

    Update 406.PCA.R

--- 406.PCA.R
+++ 406.PCA.R
@@ -3,12 +3,15 @@ library(imageRy)
 library(viridis)
 library(terra)
 
-sent <- im.import("sentinel.png")
-sent.pca <- im.pca(sent)
+sent<-im.import("sentinel.png")
+pairs(sent)  ## Possiamo vedere che il NIR è poco correlato alle altre bande,
+  ##  mentre le prime due sono molto correlate
+sentpc<-im.pca2(sent)  ##  'im.pca2' con la vecchia versione
 
-# measuring variability in the first PC:
-sd3pc <- focal(sent.pca[[1]], matrix(1/9, 3, 3), fun=sd)
-viridis <- colorRampPalette(viridis(7))(255)
+# Measuring variability in the first PC:
+pc1<-sentpc$PC1
+sd3pc<-focal(pc1, matrix(1/9, 3, 3), fun=sd)
+viridis<-colorRampPalette(viridis(7))(255)
 plot(sd3pc, col=viridis)
 
 
